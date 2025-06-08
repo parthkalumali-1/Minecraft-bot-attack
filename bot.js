39fbb0d87e2a409ee13f0efc55525d1123cf162c
Parth Kalumali <parthkalumali@gmail.com>
2025-06-08 19:58:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,26 +2,7 @@ const mineflayer = require('mineflayer');
 
 function createBot(username) {
   const bot = mineflayer.createBot({
-    host: 'play.peacesmp.fun', // replace with your IP
-    port: 25565,
-    username: username,
+    host: 'rocket33smp.aternos.me', // replace with your IP
+    port: 60999,
+    username: Yussuf_ABDUL,
     version: false // auto-detect version
-  });
-
-  bot.on('spawn', () => {
-    console.log(`${username} has joined.`);
-  });
-
-  bot.on('end', () => {
-    console.log(`${username} disconnected.`);
-  });
-
-  bot.on('error', err => {
-    console.log(`Error with ${username}:`, err);
-  });
-}
-
-// Launch 50 bots
-for (let i = 1; i <= 50; i++) {
-  createBot('Bot_' + i);
-}
